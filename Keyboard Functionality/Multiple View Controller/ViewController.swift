c115629905a29c88894a1a22d9a40de2c5d4b76c
Alish <alishgiri.92@gmail.com>
2018-03-19 08:09:25 +0545

    swift repo created

---
+++ Keyboard Functionality/Multiple View Controller/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Multiple View Controller
+//
+//  Created by Alish Giri on 2/11/18.
+//  Copyright © 2018 Alish Giri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // touchesBegan function hides the keyboard when user touches away from the keyboard and anywhere on the screen.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // textFieldShouldReturn function runs some code when user press return on the keyboard.
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        // The code below will make the keyboard disappear.
+        textField.resignFirstResponder()
+        return true
+    }
+
+
+}
+
