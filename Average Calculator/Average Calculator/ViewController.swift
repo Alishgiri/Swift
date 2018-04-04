a0175d890c2ab8ac2fc3bc59b9d5d43002824a75
Alish <alishgiri.92@gmail.com>
2018-04-04 12:22:46 +0545

    replaceOccurance of Strings

--- Average Calculator/Average Calculator/ViewController.swift
+++ Average Calculator/Average Calculator/ViewController.swift
@@ -19,7 +19,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
