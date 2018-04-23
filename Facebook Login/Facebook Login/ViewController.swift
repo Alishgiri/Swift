77496ea8290e0ec8a1bb7e58e69cbbe53ea8ac29
Alish <alishgiri.92@gmail.com>
2018-04-23 15:27:05 +0545

    Facebook login functionality, read READ ME file in frameworks

---
+++ Facebook Login/Facebook Login/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Facebook Login
+//
+//  Created by Alish Giri on 4/23/18.
+//  Copyright © 2018 Alish Giri. All rights reserved.
+//
+
+/*
+ GOTO developer.facebook.com AND DOWNLOAD THE FACEBOOK SDK AND FOLLOW THE INSTRUCTION,
+ THE RESULT WILL BE AS BELOW.
+*/
+
+import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let loginBtn = FBSDKLoginButton()
+        loginBtn.center = view.center
+        loginBtn.readPermissions = ["public_profile", "email"]
+        loginBtn.delegate = self
+        view.addSubview(loginBtn)
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print(error)
+        } else if result.isCancelled {
+            print("User has cancelled login!")
+        } else {
+            if result.grantedPermissions.contains("email") {
+                if let graphRequest = FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"email,name"]) {
+                    graphRequest.start { (connection, result, error) in
+                        if let error = error {
+                            print(error)
+                        } else {
+                            if let userDeets = result {
+                                print(userDeets)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Logged out!")
+    }
+    
+}
+
