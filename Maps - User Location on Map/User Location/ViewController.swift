e27ef73f72775512108ffcbe6ad6699fdf55cc6c
Alish <alishgiri.92@gmail.com>
2018-04-30 14:29:10 +0545

    displaying user location on map

--- Maps - User Location on Map/User Location/ViewController.swift
+++ Maps - User Location on Map/User Location/ViewController.swift
@@ -17,7 +17,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
@@ -25,11 +24,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
         locationManager.startUpdatingLocation()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let userLocation: CLLocation = locations[0]
         let latitude = userLocation.coordinate.latitude
